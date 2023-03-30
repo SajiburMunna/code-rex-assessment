393f993aef4ce9fa9efb8d9575bf79660a18e646
SajiburMunna <sajiburmunna087@gmail.com>
2023-03-30 18:09:38 +0600

    feat:create and setup code rex assessment app

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+function Layout() {
+  return (
+    <>
+      <div className="h-10 bg-blue-900">
+        <h1 className="text-white">
+          code<span className="text-sky-700">rex</span>
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
