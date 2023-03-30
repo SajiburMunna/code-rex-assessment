fbc7ce77283fe77912a5fa62a698956a46a19f15
SajiburMunna <sajiburmunna087@gmail.com>
2023-03-30 22:20:32 +0600

    style:type changed

--- @types/users.d.ts
+++ @types/users.d.ts
@@ -5,28 +5,17 @@ type Company = {
   title: string;
 };
 
-type Address = {
-  address: string;
-  city: string;
-  coordinates: Coordinates;
-  postalCode: string;
-  state: string;
-};
-
-type Coordinates = {
-  lat: number;
-  lng: number;
+type UserData = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  company: Company;
 };
 
 type Users = {
   limit: number;
   skip: number;
   total: number;
-  users: {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    company: Company;
-  }[];
+  users: UserData[];
 };
